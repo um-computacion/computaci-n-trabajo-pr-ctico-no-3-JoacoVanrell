45b30ad7b397cca4bcee0a3edc88eb6be209aa90
Joaquin Vanrell <j.vanrell@alumno.um.edu.ar>
2025-04-14 23:19:46 -0300

    Tercera issue, Fase 2

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,20 +3,20 @@ class NumeroDebeSerPositivo(Exception):
     pass
 
 def ingrese_numero():
-    pass
     
-#     Returns:
-#         int: El número ingresado si es válido.
+    """
+    Returns:
+        int: El número ingresado si es válido.
         
-#     Raises:
-#         ValueError: Si la entrada no es un número válido.
-#         NumeroDebeSerPositivo: Si el número ingresado es negativo.
-#     """
-#     entrada = input("Ingrese un número: ")
-#     try:
-#         numero = int(entrada)
-#         if numero < 0:
-#             raise NumeroDebeSerPositivo("El número debe ser positivo")
-#         return numero
-#     except ValueError:
-#         raise ValueError("La entrada debe ser un número válido") 
+    Raises:
+        ValueError: Si la entrada no es un número válido.
+        NumeroDebeSerPositivo: Si el número ingresado es negativo.
+    """
+    entrada = input("Ingrese un número: ")
+    try:
+        numero = int(entrada)
+        if numero < 0:
+            raise NumeroDebeSerPositivo("El número debe ser positivo")
+        return numero
+    except ValueError:
+        raise ValueError("La entrada debe ser un número válido") 
